45a1f393ec2bc057f0612055e4f55425f162c154
ademola <ademolaemiabata@gmail.com>
2019-03-11 22:21:47 +0000

    slick plug added

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // Jumbotron background and text scroll every 4s with Slick plug-in
+    $('.carousel').slick({
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: false,
+      draggable: false,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+    });
