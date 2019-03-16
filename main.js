b13ad8a28caf3cbe080cf8edff344b5f7441d90c
ademola <ademolaemiabata@gmail.com>
2019-03-16 15:55:07 +0000

    copied code

--- main.js
+++ main.js
@@ -1,28 +1,24 @@
 $(document).ready(function(){
-  
-    $('.carousel').slick({
-      slidesToScroll:1,
-     autoplay: true,
-      autoplaySpeed: 4000,
-      arrows: false,
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
     draggable: false,
-      pauseOnFocus: false,
-     pauseOnHover: false,
-    });
-   
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
 
-
-var  $scrollDist = 0;
-  $(window).scroll(function (){
-    var $scrollTop = $(this).scrollTop();
-{if ($scrollTop - $scrollDist > 50)
- var heightTop = $('.navbar').css('height');
-  $('.navbar').animate({top: '-' + heightTop}, 150);
- $scrollDist = $scrollTop;
-  } else if($scrollDist - $scrollTop > 50 ){
-    $('.navbar').animate({top:'0px'},150);
-    $scrollDist = $scrollTop;
-    
-}
+  var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      lastScrollTop = scrollTop;
+    } else if (lastScrollTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      lastScrollTop = scrollTop;
+    }
+  });
 });
-});  
